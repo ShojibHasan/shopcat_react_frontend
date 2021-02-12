c0e2b65b0fa098280ff4a0424ace770fde10bd9f
shojibhasan <shojibhasan15@gmail.com>
2021-02-13 03:04:25 +0600

    product page design

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container,Row,Col } from 'react-bootstrap'
+
+function Footer() {
+    return (
+        <footer>
+            <Container>
+                <Col className="text-center py-3">Copyright &copy; ShopCat</Col>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
